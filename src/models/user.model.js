02d7434d8e33dc94cf1d17bc5dd650bbc88aa378
Ni3-Jadhav <ntjadhav2000@gmail.com>
2026-06-08 13:43:23 +0530

    connect mongodb data base and create user model

---
+++ src/models/user.model.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required for user registration"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required for user registration"],
+      unique: [true, "Email already exists, please use a different email"],
+      trim: true,
+      lowercase: true,
+      match: [
+        /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+        "Please enter a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required for user registration"],
+      minlength: [6, "Password must be at least 6 characters long"],
+      trim: true,
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+  next();
+});
+
+userSchema.methods.comparePassword = async function (userPassword) {
+  return await bcrypt.compare(userPassword, this.password);
+};
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
