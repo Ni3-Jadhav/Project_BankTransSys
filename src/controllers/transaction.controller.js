fcf75ab6b552a1a9539af0dd8e1cbc4ec38484d1
Ni3-Jadhav <ntjadhav2000@gmail.com>
2026-06-16 14:07:10 +0530

    create function for check  balance in account model and controller for transaction

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -1,5 +1,6 @@
 const transactionModel = require("../models/transaction.model");
 const ledgerModel = require("../models/ledger.model");
+const accountModel = require("../models/account.model");
 
 const emailService = require("../services/email.service");
 
@@ -21,4 +22,90 @@ const emailService = require("../services/email.service");
 
 async function createTransaction(req, res) {
   const { fromAccount, toAccount, amount, idempotencyKey } = req.body;
+
+  /**
+   * - Validate request
+   */
+
+  if (!fromAccount || !toAccount || !amount || !idempotencyKey) {
+    return res.status(400).json({
+      message:
+        "fromAccont, toAccount, amount and idempotencyKey all are required for transaction",
+    });
+  }
+
+  const fromUserAccount = await accountModel.findOne({
+    _id: fromAccount,
+  });
+
+  const toUserAccount = await accountModel.findOne({
+    _id: toAccount,
+  });
+
+  if (!fromUserAccount || !toUserAccount) {
+    return res.status(400).json({
+      meassage: "Invalid fromAccount and toAccount",
+    });
+  }
+
+  /**
+   * - Validate idempotency key
+   */
+
+  const isTransactionAlreadyExists = await transactionModel.findOne({
+    idempotencyKey: idempotencyKey,
+  });
+
+  if (isTransactionAlreadyExists) {
+    if (isTransactionAlreadyExists.status === "COMPLETED") {
+      return res.status(200).json({
+        message: "Transaction Already processed",
+        transaction: isTransactionAlreadyExists,
+      });
+    }
+
+    if (isTransactionAlreadyExists.status === "PENDING") {
+      return res.status(200).json({
+        message: "Transaction is still processing",
+      });
+    }
+
+    if (isTransactionAlreadyExists.status === "FAILED") {
+      return res.status(500).json({
+        message: "Transaction processing failed, please retry",
+      });
+    }
+
+    if (isTransactionAlreadyExists.status === "REFUNDED") {
+      return res.status(500).json({
+        message: "Transaction processed with Refund",
+      });
+    }
+  }
+
+  /**
+   * - Check account status
+   */
+
+  if (
+    fromUserAccount.status !== "ACTIVE" ||
+    toUserAccount.status !== "ACTIVE"
+  ) {
+    return res.status(400).json({
+      message:
+        "Both fromAccount and toAccount must be ACTIVE to process transaction",
+    });
+  }
+
+  /**
+   * - Derive sender balance from ledger
+   */
+
+  const senderBalance = await fromUserAccount.getBalance();
+
+  if (senderBalance < amount) {
+    return res.status(400).json({
+      message: `Insufficient balance. Current balance is ${senderBalance}. Requested amount is ${amount}`,
+    });
+  }
 }
